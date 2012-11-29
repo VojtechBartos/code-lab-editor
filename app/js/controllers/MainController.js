eb54b0a4876bf7129ffb4b4e417a9e6e5f08ecb9
Vojtech Bartos <me@vojtechbartos.cz>
2012-11-29 10:19:12 +0100

    remove function randomString

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -3,19 +3,8 @@
 codeEditor.controller('MainController', function MainController($scope, $location) {
 
     $scope.createNewEditor = function(){
-    	function randomString(length) {
-		    var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz'.split('');
-		    if (! length) length = Math.floor(Math.random() * chars.length);
-		    var str = '';
-		    for (var i = 0; i < length; i++) {
-		        str += chars[Math.floor(Math.random() * chars.length)];
-		    }
-		    return str;
-		}
-
-		// redirect
+    	// redirect
 		$location.path('/editor/' + randomString(6));
-
     };	
 
 });
